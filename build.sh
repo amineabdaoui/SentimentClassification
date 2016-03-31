53a8cb0108cf475dfa86a51d772c97f3639501bb
Vincent Emonet <vincent.emonet@gmail.com>
2016-03-31 17:17:45 +0200

    stop adding the jar to tomcat lib (maven local repo is enough)

--- build.sh
+++ build.sh
@@ -6,6 +6,3 @@ mvn clean package
 # In case you need to clean the local repository (if not updating well)
 #mvn dependency:purge-local-repository
 mvn install:install-file -Dfile=target/sentiment-classification.jar -DgroupId=fr.advanse.lirmm -DartifactId=SentimentClassification -Dversion=0.1 -Dpackaging=jar -DgeneratePom=true
-        
-sudo cp target/sentiment-classification.jar /opt/tomcat/lib/sentiment-classification.jar
-sudo chown tomcat:tomcat /opt/tomcat/lib/sentiment-classification.jar
