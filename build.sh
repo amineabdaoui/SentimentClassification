96e8ea54aab96f4f65df3473919840cb84fea8e5
Vincent Emonet <vincent.emonet@gmail.com>
2016-03-31 16:14:23 +0200

    use relative path to add sentiment-classification.jar to maven local repo

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mvn clean package
 
 # In case you need to clean the local repository (if not updating well)
 #mvn dependency:purge-local-repository
-mvn install:install-file -Dfile=/home/emonet/java_workspace/SentimentClassification/target/sentiment-classification.jar -DgroupId=fr.advanse.lirmm -DartifactId=SentimentClassification -Dversion=0.1 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=target/sentiment-classification.jar -DgroupId=fr.advanse.lirmm -DartifactId=SentimentClassification -Dversion=0.1 -Dpackaging=jar -DgeneratePom=true
         
 sudo cp target/sentiment-classification.jar /opt/tomcat/lib/sentiment-classification.jar
 sudo chown tomcat:tomcat /opt/tomcat/lib/sentiment-classification.jar
